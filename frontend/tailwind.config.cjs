86dd1e7a38c5a8b73aba8085358105da503d8314
kariuki <wangaripekariuki@gmail.com>
2026-03-06 08:58:26 +0300

    chore(frontend): add frontend/tailwind.config.cjs

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {}, // extend colors if you need custom ones
+  },
+  plugins: [],
+};
