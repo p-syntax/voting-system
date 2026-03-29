62febcccec1be202777564da7ff0953d7b856a2f
kariuki <wangaripekariuki@gmail.com>
2026-03-29 22:54:54 +0300

    the main page in frontend contains the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,38 @@
-// src/App.jsx
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import CheckPermission from './context/checkRole.jsx';
-// import { AdminProvider } from './context/AdminContext';
-// import { VoterProvider } from './context/VoterContext';
 
-// // Admin Pages
-// import AdminLogin from './pages/admin/AdminLogin';
-// import AdminDashboard from './pages/admin/Dashboard';
-// import AdminLayout from './layouts/AdminLayout';
-
-// Voter Pages
 import VoterLogin from './pages/voter/login.jsx';
 import AdminLogin from './pages/Admin/login.jsx';
 import AdminDashboard from './pages/Admin/dashboard.jsx';
-import VoterRegistration from './pages/Admin/voterRegistration.jsx';
 import VotingPage from './pages/voter/votingPage.jsx';
-import AddContestant from './pages/Admin/ContestantRegistration.jsx';
-// import VotingInterface from './pages/voter/VotingInterface';
-// import ThankYou from './pages/voter/ThankYou';
-
-// import './App.css';
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    
-   
+    <>
+      {/* Correct placement */}
+      <Toaster position="top-right" />
+
       <Routes>
-            {/* Default Route - Voter Login */}
-            <Route path='/' element={<VoterLogin />} />
-             {/* Default Route - Voter Login */}
-            <Route path='/votingPage' element={<VotingPage/>} />
-           
-            {/*admin routes */}
-            <Route path='/admin/login' element={<AdminLogin />} />
-            <Route
-              path='/admin/register'
-              element={
-                <CheckPermission allowedRoles={["admin"]}>
-                  <VoterRegistration />
-                </CheckPermission>
-              }
-            />
-            <Route
-              path='/admin/AddContestant'
-              element={
-                <CheckPermission allowedRoles={["admin"]}>
-                  <AddContestant/>
-                </CheckPermission>
-              }
-            />
-            <Route
-              path='/admin/dashboard'
-              element={
-                <CheckPermission allowedRoles={["admin"]}>
-                 <AdminDashboard/>
-                </CheckPermission>
-              }
-            />
-            
-      </Routes >     
-   
+        {/* Default Route - Voter Login */}
+        <Route path='/' element={<VoterLogin />} />
+
+        {/* Voting Page */}
+        <Route path='/votingPage' element={<VotingPage />} />
+
+        {/* Admin routes */}
+        <Route path='/admin/login' element={<AdminLogin />} />
+
+        <Route
+          path='/admin/dashboard'
+          element={
+            <CheckPermission allowedRoles={["admin"]}>
+              <AdminDashboard />
+            </CheckPermission>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
