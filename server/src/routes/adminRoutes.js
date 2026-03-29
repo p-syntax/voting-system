1e3cdeb9d6b909539205fd48910dbc242a418f67
kariuki <wangaripekariuki@gmail.com>
2026-03-29 23:56:42 +0300

    added the delete contestants route

--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import {voterRegistration,getVoters,addContestant,getContestant,updateVoter } from "../controllers/adminControllers.js"
+import {voterRegistration,getVoters,addContestant,getContestant,updateVoter,updateContestant,setVotingTime,deleteContestant } from "../controllers/adminControllers.js"
 import { authenticate } from "../middlewares/authMiddleware.js";
+import { upload } from "../middlewares/upload.js";
 import { admin } from "../middlewares/authorizationMiddleware.js";
 const router = express.Router()
 router.use( authenticate,admin)
@@ -10,7 +11,11 @@ router.post('/registervoter',voterRegistration);
 router.get('/voters',getVoters);
 router.post('/updatevoter',updateVoter);
 //contestants routes 
-router.post('/addContestant',addContestant);
+router.post('/addContestant',upload.single("image"),addContestant);
 router.get('/getcontestant',getContestant);
+router.put("/updateContestant/:id", upload.single("image"), updateContestant);
+router.delete("/deleteContestant/:id", deleteContestant);
+//update voting window 
+router.post('/setVotingTime',setVotingTime)
 
 export default router;
