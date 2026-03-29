d38ccfef042677dd7dc6f540399159566b15cec4
kariuki <wangaripekariuki@gmail.com>
2026-03-30 00:01:39 +0300

    voter routes

--- server/src/routes/voterRoutes.js
+++ server/src/routes/voterRoutes.js
@@ -2,12 +2,13 @@ import express from "express"
 import { Vote,getContestants,completeVoting, } from "../controllers/voterController.js"
 import { authenticate } from "../middlewares/authMiddleware.js"
 import { voter } from "../middlewares/authorizationMiddleware.js"
+import { checkVotingWindow } from "../middlewares/checkVotingTime.js"
 const router =express.Router()
 router.use(authenticate,voter)
 
 //voter routes ;
-router.post('/vote',Vote);
-router.get('getcontestants',getContestants);
-router.get('/completeVoting',completeVoting);
+router.post('/vote',checkVotingWindow,Vote);
+router.get('/getcontestants',checkVotingWindow,getContestants);
+router.get('/completeVoting',checkVotingWindow,completeVoting);
 
 export default router;
